29672280a2064be0a96fdde55bd049f51f434485
MarceloWyse <marceloamaral88@gmail.com>
2022-12-19 09:04:42 -0300

    criar videos

---
+++ js/conectaAPI.js
@@ -0,0 +1,33 @@
+async function listaVideos() {
+    const fetchAPI = await fetch('http://localhost:3000/videos');
+    const fetchAPIConvertida = await fetchAPI.json();
+    return fetchAPIConvertida;
+}
+
+async function criaVideos(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                titulo: titulo,
+                descricao: `${descricao} visualizações`,
+                url: url,
+                imagem: imagem
+            }
+        )
+    })
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+export const conectaAPI = {
+    listaVideos,
+    criaVideos
+};
+
+
+
